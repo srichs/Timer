9b59157cc8069ac63bf909cd949f02945d9e1e14
srichs <sr@srich.us>
2021-01-02 14:19:18 -0500

    Updated indentations

--- Timer.cpp
+++ Timer.cpp
@@ -9,54 +9,54 @@
 
 void Timer::countdown(Fidelity fidelity, uint16_t digits)
 {
-    _fidelity = fidelity;
-    _type = COUNTDOWN;
-    setTime(digits);
-    init();
+  _fidelity = fidelity;
+  _type = COUNTDOWN;
+  setTime(digits);
+  init();
 }
 
 void Timer::countup(Fidelity fidelity)
 {
-    _fidelity = fidelity;
-    _type = COUNTUP;
-    _digitOne = 0;
-    _digitTwo = 0;
-    _digitThree = 0;
-    _digitFour = 0;
-    init();
+  _fidelity = fidelity;
+  _type = COUNTUP;
+  _digitOne = 0;
+  _digitTwo = 0;
+  _digitThree = 0;
+  _digitFour = 0;
+  init();
 }
 
 void Timer::update()
 {
-    if (millis() >= _nextTime && !_isPaused) {
-        if (_type == COUNTDOWN) {
-            decrement();
-        } else {
-            increment();
-        }
-        _display.sendDigits(_digitOne, _digitTwo, _digitThree, _digitFour, 1);
-        if (_fidelity == MINUTES) {
-            _nextTime += MINUTE_INCREMENT;
-        } else {
-            _nextTime += SECOND_INCREMENT;
-        }
+  if (millis() >= _nextTime && !_isPaused) {
+    if (_type == COUNTDOWN) {
+      decrement();
+    } else {
+      increment();
     }
+    _display.sendDigits(_digitOne, _digitTwo, _digitThree, _digitFour, 1);
+    if (_fidelity == MINUTES) {
+      _nextTime += MINUTE_INCREMENT;
+    } else {
+      _nextTime += SECOND_INCREMENT;
+    }
+  }
 }
 
 void Timer::pause()
 {
-    if (!_isPaused) {
-        _isPaused = true;
-        _pauseTime = _nextTime - millis();
-    }
+  if (!_isPaused) {
+    _isPaused = true;
+    _pauseTime = _nextTime - millis();
+  }
 }
 
 void Timer::restart()
 {
-    if (_isPaused) {
-        _isPaused = false;
-        _nextTime = millis() + _pauseTime;
-    }
+  if (_isPaused) {
+    _isPaused = false;
+    _nextTime = millis() + _pauseTime;
+  }
 }
 
 /*
@@ -65,19 +65,19 @@ void Timer::restart()
 void Timer::init()
 {
 #if (TIMER_DEBUG == 1)
-    Serial.begin(9600);
-    delay(1000);
-    Serial.println("Timer initialization");
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Timer initialization");
 #endif
-    _done = false;
-    _isPaused = false;
-    _display.initDisplay();
-    _display.sendDigits(_digitOne, _digitTwo, _digitThree, _digitFour, 1);
-    if (_fidelity == MINUTES) {
-        _nextTime = millis() + MINUTE_INCREMENT;
-    } else {
-        _nextTime = millis() + SECOND_INCREMENT;
-    }
+  _done = false;
+  _isPaused = false;
+  _display.initDisplay();
+  _display.sendDigits(_digitOne, _digitTwo, _digitThree, _digitFour, 1);
+  if (_fidelity == MINUTES) {
+    _nextTime = millis() + MINUTE_INCREMENT;
+  } else {
+    _nextTime = millis() + SECOND_INCREMENT;
+  }
 }
 
 /*
@@ -85,27 +85,27 @@ void Timer::init()
  */
 void Timer::setTime(uint16_t digits)
 {
-    if (digits > 9999 || digits <= 0) {
-    #if (TIMER_DEBUG == 1)
-        Serial.println("Given time outside of range.");
-    #endif
-    } else {
-        _digitFour = digits % 10;
-        digits = digits / 10;
-        _digitThree = digits % 10;
-        digits = digits / 10;
-        _digitTwo = digits % 10;
-        digits = digits / 10;
-        _digitOne = digits % 10;
-    #if (TIMER_DEBUG == 1)
-        Serial.print("Time Set: ");
-        Serial.print(_digitOne);
-        Serial.print(_digitTwo);
-        Serial.print(":");
-        Serial.print(_digitThree);
-        Serial.println(_digitFour);
-    #endif
-    }
+  if (digits > 9999 || digits <= 0) {
+  #if (TIMER_DEBUG == 1)
+    Serial.println("Given time outside of range.");
+  #endif
+  } else {
+    _digitFour = digits % 10;
+    digits = digits / 10;
+    _digitThree = digits % 10;
+    digits = digits / 10;
+    _digitTwo = digits % 10;
+    digits = digits / 10;
+    _digitOne = digits % 10;
+  #if (TIMER_DEBUG == 1)
+    Serial.print("Time Set: ");
+    Serial.print(_digitOne);
+    Serial.print(_digitTwo);
+    Serial.print(":");
+    Serial.print(_digitThree);
+    Serial.println(_digitFour);
+  #endif
+  }
 }
 
 /*
@@ -113,16 +113,16 @@ void Timer::setTime(uint16_t digits)
  */
 void Timer::decrement()
 {
-    if (_digitFour == 0) {
-        if (_digitThree == 0) {
-            decrementLarge();
-        } else {
-            _digitThree--;
-            _digitFour = 9;
-        }
+  if (_digitFour == 0) {
+    if (_digitThree == 0) {
+      decrementLarge();
     } else {
-        _digitFour--;
+      _digitThree--;
+      _digitFour = 9;
     }
+  } else {
+    _digitFour--;
+  }
 }
 
 /*
@@ -130,20 +130,20 @@ void Timer::decrement()
  */
 void Timer::decrementLarge()
 {
-    if (_digitTwo == 0) {
-        if (_digitOne == 0) {
-            alarm();
-        } else {
-            _digitOne--;
-            _digitTwo = 9;
-            _digitThree = 5;
-            _digitFour = 9;
-        }
+  if (_digitTwo == 0) {
+    if (_digitOne == 0) {
+      alarm();
     } else {
-        _digitTwo--;
-        _digitThree = 5;
-        _digitFour = 9;
+      _digitOne--;
+      _digitTwo = 9;
+      _digitThree = 5;
+      _digitFour = 9;
     }
+  } else {
+    _digitTwo--;
+    _digitThree = 5;
+    _digitFour = 9;
+  }
 }
 
 /*
@@ -151,16 +151,16 @@ void Timer::decrementLarge()
  */
 void Timer::increment()
 {
-    if (_digitFour == 9) {
-        if (_digitThree == 5) {
-            incrementLarge();
-        } else {
-            _digitThree++;
-            _digitFour = 0;
-        }
+  if (_digitFour == 9) {
+    if (_digitThree == 5) {
+      incrementLarge();
     } else {
-        _digitFour++;
+      _digitThree++;
+      _digitFour = 0;
     }
+  } else {
+    _digitFour++;
+  }
 }
 
 /*
@@ -168,24 +168,24 @@ void Timer::increment()
  */
 void Timer::incrementLarge()
 {
-    if (_digitTwo == 9) {
-        if (_digitOne == 9) {
-            _digitOne = 0;
-            _digitTwo = 0;
-            _digitThree = 0;
-            _digitFour = 0;
-            alarm();
-        } else {
-            _digitOne++;
-            _digitTwo = 0;
-            _digitThree = 0;
-            _digitFour = 0;
-        }
+  if (_digitTwo == 9) {
+    if (_digitOne == 9) {
+      _digitOne = 0;
+      _digitTwo = 0;
+      _digitThree = 0;
+      _digitFour = 0;
+      alarm();
     } else {
-        _digitTwo++;
-        _digitThree = 0;
-        _digitFour = 0;
+      _digitOne++;
+      _digitTwo = 0;
+      _digitThree = 0;
+      _digitFour = 0;
     }
+  } else {
+    _digitTwo++;
+    _digitThree = 0;
+    _digitFour = 0;
+  }
 }
 
 /*
@@ -194,10 +194,10 @@ void Timer::incrementLarge()
  */
 void Timer::alarm()
 {
-    if (!_done) {
-        digitalWrite(_alarmPin, HIGH);
-        delay(ALARM_LENGTH);
-        digitalWrite(_alarmPin, LOW);
-        _done = true;
-    }
+  if (!_done) {
+    digitalWrite(_alarmPin, HIGH);
+    delay(ALARM_LENGTH);
+    digitalWrite(_alarmPin, LOW);
+    _done = true;
+  }
 }
